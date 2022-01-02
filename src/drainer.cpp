8e1012f670fec92524e45764c30a47c490d98246
Injae Kang <abcinje@gmail.com>
2022-01-03 04:23:44 +0900

    Use shared memory among processes

--- src/drainer.cpp
+++ src/drainer.cpp
@@ -1,4 +1,41 @@
+#include <cerrno>
+#include <cstdlib>
+#include <cstring>
+#include <stdexcept>
+#include <string>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#define SHM_NAME "ckptfs"
+#define SHM_SIZE 4096
+
 int main(void)
 {
+	void *shm;
+	int shm_fd;
+
+	if ((shm_fd = shm_open(SHM_NAME, O_RDWR | O_CREAT | O_EXCL, 0664)) == -1)
+		throw std::runtime_error("shm_open() failed (" + std::string(strerror(errno)) + ")");
+
+	if (ftruncate(shm_fd, SHM_SIZE) == -1)
+		throw std::runtime_error("ftruncate() failed (" + std::string(strerror(errno)) + ")");
+
+	if ((shm = mmap(nullptr, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0)) == MAP_FAILED)
+		throw std::runtime_error("mmap() failed (" + std::string(strerror(errno)) + ")");
+
+	/* TODO: drain */
+
+	if (shm_unlink(SHM_NAME) == -1)
+		throw std::runtime_error("shm_unlink() failed (" + std::string(strerror(errno)) + ")");
+
+	if (munmap(shm, SHM_SIZE) == -1)
+		throw std::runtime_error("munmap() failed (" + std::string(strerror(errno)) + ")");
+
+	if (close(shm_fd) == -1)
+		throw std::runtime_error("close() failed (" + std::string(strerror(errno)) + ")");
+
 	return 0;
 }
--- src/hook.cpp
+++ src/hook.cpp
@@ -1,7 +1,47 @@
+#include <cerrno>
+#include <cstring>
+#include <string>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#include "util.hpp"
+
+#define SHM_NAME "ckptfs"
+#define SHM_SIZE 4096
+
+void *shm;
+
+static void init_shm(void)
+{
+	int shm_fd;
+
+	shm_fd = shm_open(SHM_NAME, O_RDWR, 0664);
+	if (shm_fd == -1)
+		error("shm_open() failed (" + std::string(strerror(errno)) + ")");
+
+	shm = mmap(nullptr, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	if (shm == MAP_FAILED)
+		error("mmap() failed (" + std::string(strerror(errno)) + ")");
+
+	if (close(shm_fd) == -1)
+		error("close() failed (" + std::string(strerror(errno)) + ")");
+}
+
+static void exit_shm(void)
+{
+	if (munmap(shm, SHM_SIZE) == -1)
+		error("munmap() failed (" + std::string(strerror(errno)) + ")");
+}
+
 static __attribute__((constructor)) void init(void)
 {
+	init_shm();
 }
 
 static __attribute__((destructor)) void exit(void)
 {
+	exit_shm();
 }
