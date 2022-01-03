88ec61155c7548b327528e9a2b581ec83e7a6289
Injae Kang <abcinje@gmail.com>
2022-01-03 20:32:27 +0900

    Install a signal handler for SIGINT

--- src/drainer.cpp
+++ src/drainer.cpp
@@ -1,6 +1,8 @@
 #include <cerrno>
+#include <csignal>
 #include <cstdlib>
 #include <cstring>
+#include <iostream>
 #include <stdexcept>
 #include <string>
 
@@ -12,10 +14,33 @@
 #define SHM_NAME "ckptfs"
 #define SHM_SIZE 4096
 
+void *shm;
+
+static void sigint_handler(int signum)
+{
+	std::cout << "Terminating." << std::endl;
+
+	if (shm_unlink(SHM_NAME) == -1)
+		throw std::runtime_error("shm_unlink() failed (" + std::string(strerror(errno)) + ")");
+
+	/* TODO: clean up */
+
+	if (munmap(shm, SHM_SIZE) == -1)
+		throw std::runtime_error("munmap() failed (" + std::string(strerror(errno)) + ")");
+
+	exit(EXIT_SUCCESS);
+}
+
+static void do_drain(void)
+{
+}
+
 int main(void)
 {
-	void *shm;
 	int shm_fd;
+
+	if (signal(SIGINT, sigint_handler) == SIG_ERR)
+		throw std::runtime_error("signal() failed (" + std::string(strerror(errno)) + ")");
 
 	if ((shm_fd = shm_open(SHM_NAME, O_RDWR | O_CREAT | O_EXCL, 0664)) == -1)
 		throw std::runtime_error("shm_open() failed (" + std::string(strerror(errno)) + ")");
@@ -26,16 +51,9 @@ int main(void)
 	if ((shm = mmap(nullptr, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0)) == MAP_FAILED)
 		throw std::runtime_error("mmap() failed (" + std::string(strerror(errno)) + ")");
 
-	/* TODO: drain */
-
-	if (shm_unlink(SHM_NAME) == -1)
-		throw std::runtime_error("shm_unlink() failed (" + std::string(strerror(errno)) + ")");
-
-	if (munmap(shm, SHM_SIZE) == -1)
-		throw std::runtime_error("munmap() failed (" + std::string(strerror(errno)) + ")");
-
 	if (close(shm_fd) == -1)
 		throw std::runtime_error("close() failed (" + std::string(strerror(errno)) + ")");
 
-	return 0;
+	while (true)
+		do_drain();
 }
