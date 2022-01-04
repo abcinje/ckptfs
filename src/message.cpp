a5dcebcb1b4dc06772fe90b2780f520997a18bd2
Injae Kang <abcinje@gmail.com>
2022-01-04 16:13:09 +0900

    Add a pathname member to the message class

--- src/message.cpp
+++ src/message.cpp
@@ -1,6 +1,9 @@
+#include <cstring>
+
 #include "message.hpp"
 
-message::message(long syscall, pid_t pid, int fd, off_t offset, size_t len)
+message::message(long syscall, const char *path, pid_t pid, int fd, off_t offset, size_t len)
 		: syscall(syscall), pid(pid), fd(fd), offset(offset), len(len)
 {
+	strncpy(this->path, path, PATH_MAX);
 }
--- src/message.hpp
+++ src/message.hpp
@@ -1,17 +1,20 @@
 #ifndef CKPTFS_MESSAGE_HPP
 #define CKPTFS_MESSAGE_HPP
 
+#include <climits>
+
 #include <sys/types.h>
 
 class message {
 public:
 	long syscall;
+	char path[PATH_MAX];
 	pid_t pid;
 	int fd;
 	off_t offset;
 	size_t len;
 
-	message(long syscall, pid_t pid, int fd, off_t offset, size_t len);
+	message(long syscall, const char *path, pid_t pid, int fd, off_t offset, size_t len);
 };
 
 #endif //CKPTFS_MESSAGE_HPP
