8b5ee2fac3cb8c67585a44735e912649a685ec22
Injae Kang <abcinje@gmail.com>
2022-01-04 16:17:04 +0900

    Initialize the pathname only for open() syscalls

--- src/message.cpp
+++ src/message.cpp
@@ -1,9 +1,12 @@
 #include <cstring>
 
+#include <syscall.h>
+
 #include "message.hpp"
 
 message::message(long syscall, const char *path, pid_t pid, int fd, off_t offset, size_t len)
 		: syscall(syscall), pid(pid), fd(fd), offset(offset), len(len)
 {
-	strncpy(this->path, path, PATH_MAX);
+	if (syscall == SYS_open)
+		strncpy(this->path, path, PATH_MAX);
 }
