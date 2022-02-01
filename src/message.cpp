8ab9b5ab25aba6ad3a8640fb76828b2490a65dca
Injae Kang <abcinje@gmail.com>
2022-02-01 19:39:28 +0900

    Add syscall_flag class

--- src/message.cpp
+++ src/message.cpp
@@ -8,7 +8,7 @@ message::message(void)
 {
 }
 
-message::message(long syscall, uint64_t ofid, off_t offset, size_t len, handle_vec handles)
-		: syscall(syscall), ofid(ofid), offset(offset), len(len), handles(handles)
+message::message(long syscall, uint64_t ofid, off_t offset, size_t len, handle_vec handles, syscall_flag flags)
+		: syscall(syscall), ofid(ofid), offset(offset), len(len), handles(handles), flags(flags)
 {
 }
--- src/message.hpp
+++ src/message.hpp
@@ -5,6 +5,8 @@
 
 #include <boost/interprocess/managed_shared_memory.hpp>
 
+#include "syscall_flag.hpp"
+
 namespace bi = boost::interprocess;
 using shm_handle = bi::managed_shared_memory::handle_t;
 
@@ -17,9 +19,10 @@ public:
 	struct handle_vec {
 		shm_handle fq_handle, pathname_handle, synced_handle;
 	} handles;
+	syscall_flag flags;
 
 	message(void);
-	message(long syscall, uint64_t ofid, off_t offset, size_t len, handle_vec handles = {});
+	message(long syscall, uint64_t ofid, off_t offset, size_t len, handle_vec handles = {}, syscall_flag flags = syscall_flag::FSYNC_NORMAL);
 };
 
 #endif //CKPTFS_MESSAGE_HPP
